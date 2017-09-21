0b5be39abc8114e0859067859edb3fb03ebcccce
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2017-09-21 16:40:33 -0400

    PropogateFn updates nodes in breadth first order with a function taking the old value and the node from which the function propagated

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 let els = []
 for (var i = 1; i < 7; i++) {
   for (var j = 0; j < i; j++) {
-    if(i % j === 0) {
+    if (i % j === 1) {
       els.push({ data: { id: String(i) + String(j), source: String(i), target: String(j) } })
     }
   }
 
   els.push({
-    data: { id: String(i) }
+    data: { id: i, num: i }
   })
 }
 
@@ -18,8 +18,8 @@ var cy = cytoscape({
     {
       selector: 'node',
       style: {
-        'background-color': '#666',
-        'label': 'data(id)'
+        'background-color': '#555',
+        'label': 'data(num)'
       }
     },
 
@@ -27,6 +27,7 @@ var cy = cytoscape({
       selector: 'edge',
       style: {
         'width': 3,
+        'curve-style': 'bezier',
         'line-color': '#ccc',
         'target-arrow-color': '#ccc',
         'target-arrow-shape': 'triangle'
@@ -34,6 +35,29 @@ var cy = cytoscape({
     }
   ],
   layout: {
-    name: 'cose'
+    name: 'breadthfirst',
+    rows: 3,
+    directed: true
   }
+});
+
+
+function propagateFn(cyto, startNode, newVal, func) {
+  startNode.data('num', newVal)
+
+  cyto.elements().bfs({
+    root: startNode,
+    visit: func,
+    directed: true
+  })
+}
+
+function update(node, edgeTraversed, nodeFrom) {
+  const oldVal = node.data('num')
+  node.data('num', nodeFrom ? oldVal + nodeFrom.data('num') : oldVal)
+}
+
+cy.on('tap', 'node', function(evt){
+  var node = evt.target;
+  propagateFn(cy, node, node.data('num'), update)
 });
