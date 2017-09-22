ea3afaea8f66d2677ca9beeccb8c5f928d59913b
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2017-09-22 10:27:12 -0400

    Uses local copy of cytoscape.js instead of CDN

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 let els = []
 for (var i = 1; i < 7; i++) {
-  for (var j = 0; j < i; j++) {
-    if (i % j === 1) {
-      els.push({ data: { id: String(i) + String(j), source: String(i), target: String(j) } })
+  for (var j = 1; j < i; j++) {
+    if (i % (j + 1) === 0) {
+      els.push({ 
+        data: { id: String(i) + String(j), source: String(i), target: String(j) } 
+      })
     }
   }
 
   els.push({
-    data: { id: i, num: i }
+    data: { id: i, nums: [i], prevVal: null }
   })
 }
 
@@ -19,7 +21,7 @@ var cy = cytoscape({
       selector: 'node',
       style: {
         'background-color': '#555',
-        'label': 'data(num)'
+        'label': 'data(nums)'
       }
     },
 
@@ -41,23 +43,39 @@ var cy = cytoscape({
   }
 });
 
-
 function propagateFn(cyto, startNode, newVal, func) {
-  startNode.data('num', newVal)
+  startNode.data('nums', newVal)
 
   cyto.elements().bfs({
     root: startNode,
-    visit: func,
+    visit: function(node, edgeTraversed, prevNode, index){
+
+      var len = node.data('nums').length
+      const prevNodeOldVal = node.data('prevVal')[len - 1] 
+
+      console.log(node.data('nums'), prevNodeOldVal)
+
+      func(node, edgeTraversed, prevNode, prevNodeOldVal)
+    },
     directed: true
   })
 }
 
-function update(node, edgeTraversed, nodeFrom) {
-  const oldVal = node.data('num')
-  node.data('num', nodeFrom ? oldVal + nodeFrom.data('num') : oldVal)
+function update(node, edgeTraversed, prevNode, prevNodeOldVal) {
+  const oldVal = node.data('nums')
+  node.data('nums', index > 0 ? oldVal.concat(prevNodeOldVal) : oldVal)
 }
 
 cy.on('tap', 'node', function(evt){
   var node = evt.target;
-  propagateFn(cy, node, node.data('num'), update)
+  propagateFn(cy, node, node.data('nums'), update)
+});
+function update(node, edgeTraversed, prevNode, prevNodeOldVal) {
+  const oldVal = node.data('nums')
+  node.data('nums', index > 0 ? oldVal.concat(prevNodeOldVal) : oldVal)
+}
+
+cy.on('tap', 'node', function(evt){
+  var node = evt.target;
+  propagateFn(cy, node, node.data('nums'), update)
 });
